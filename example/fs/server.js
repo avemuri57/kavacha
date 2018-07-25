aa758e094af6fcb5a838eb78bc584a9a8e48a027
Anand Vemuri <avemuri57@gmail.com>
2018-07-24 20:28:42 -0700

    Adding vuln fs example

--- example/fs/server.js
+++ example/fs/server.js
@@ -1,1 +1,25 @@
-// TODO
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+
+
+app.get("/",(req,res) => {
+	res.send("Hello World");
+});
+
+app.get("/read/:path",(req,res) => {
+	var x = fs.readFile(req.params.path,(err,file) => {
+		if(err){
+			res.send(err);
+		}else{
+			res.send(file);
+		}
+	})
+	
+});
+
+
+app.listen(8081, () => {
+	console.log("Listening on 8081");
+})
